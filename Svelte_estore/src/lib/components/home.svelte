d3697de7749ace2123f1309ed5cff0a35e9514d0
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-08-02 15:51:35 +0200

    added script code of the home component

---
+++ Svelte_estore/src/lib/components/home.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let products = [];
+    export let loading = true;
+    export let selectedCategory = "";
+    export let sortOrder = "";
+    export let filteredProductsList = [];
+    export let categories = [];
+    export let goToProductDetails;
+  
+    function filterProducts() {
+      let tempProducts = products;
+  
+      if (selectedCategory) {
+        tempProducts = tempProducts.filter(
+          (product) => product.category === selectedCategory
+        );
+      }
+  
+      if (sortOrder === "asc") {
+        tempProducts = tempProducts.sort((a, b) => a.price - b.price);
+      } else if (sortOrder === "desc") {
+        tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+      }
+  
+      filteredProductsList = tempProducts;
+    }
+  
+    function resetFilters() {
+      selectedCategory = "";
+      sortOrder = "";
+      filteredProductsList = products;
+    }
+  </script>
