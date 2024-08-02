4174d1947078cc048de3d22d82e2b3d4d1b26edd
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-08-02 16:55:50 +0200

    editted the onMount function

--- Svelte_estore/src/App.svelte
+++ Svelte_estore/src/App.svelte
@@ -1,10 +1,15 @@
 <script>
-    import { onMount } from 'svelte';
+   import { onMount } from 'svelte';
+
     import { Router, Route, Link } from "svelte-routing";
     import Home from './lib/components/Home.svelte';
     import ProductDetails from './lib/components/ProductDetails.svelte';
   
     export let url = "";
+
+    onMount(() => {
+    console.log('Component has been mounted');
+  });
 
     function fetchProducts() {
     loading = true;
