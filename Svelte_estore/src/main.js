af4eb6e0991041baaf2b382e1a906e3025a5783e
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-08-02 13:51:07 +0200

    editted main.js file

--- Svelte_estore/src/main.js
+++ Svelte_estore/src/main.js
@@ -1,8 +1,12 @@
 import './app.css'
 import App from './App.svelte'
+import { Router } from 'svelte-routing'
 
 const app = new App({
-  target: document.getElementById('app'),
+  target: document.body,
+  props: {
+    url: ''
+  }
 })
 
 export default app
