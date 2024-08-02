c20322879f5f4483b9805bbae6b0b4801cfbe038
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-08-02 13:53:08 +0200

    imports and export added to file

---
+++ Svelte_estore/src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { onMount } from 'svelte';
+    import { Router, Route, Link } from "svelte-routing";
+    import Home from './lib/components/Home.svelte';
+    import ProductDetails from './lib/components/ProductDetails.svelte';
+  
+    export let url = "";
