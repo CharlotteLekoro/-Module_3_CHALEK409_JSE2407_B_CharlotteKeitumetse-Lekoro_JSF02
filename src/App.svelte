3d476e25434a54bdd50c7f3e54c8417e30b2d28a
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-08-05 10:08:30 +0200

    changed the hover effect

--- src/App.svelte
+++ src/App.svelte
@@ -42,12 +42,12 @@
         <Link to="/">
           <button class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="./public/logo.png" class="h-8" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap text-primary-light">Emercom</span>
+            <span class="self-center text-2xl font-semibold whitespace-nowrap text-primary-light">Trendy Treasures</span>
           </button>
         </Link>
         <button
           on:click={() => open = !open}
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
           aria-controls="navbar-default"
           aria-expanded="false"
         >
